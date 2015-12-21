e293d6a088ab6cd7921ac2bb22ce8059baa2af1f
Tyler <tyler.rinker@gmail.com>
2015-12-21 00:48:50 -0500

    fixed non ascci in data

---
+++ inst/data_sets.R
@@ -0,0 +1,24 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(rvest, magrittr, xml2)
+
+debates <- c(
+    wisconsin = "110908",
+    boulder = "110906",
+    california = "110756",
+    ohio = "110489"
+)
+
+partial_republican_debates_2015 <- lapply(debates, function(x){
+    xml2::read_html(paste0("http://www.presidency.ucsb.edu/ws/index.php?pid=", x)) %>%
+        rvest::html_nodes("p") %>%
+        rvest::html_text() %>%
+        textshape::split_index(., grep("^[A-Z]+:", .)) %>%
+        #textshape::split_match("^[A-Z]+:", TRUE, TRUE) %>% #equal to line above
+        textshape::combine() %>%
+        textshape::split_transcript() %>%
+        textshape::split_sentence()
+}) %>%
+    textshape::bind_list("location") %>%
+    qdapRegex::rm_non_ascii()
+
+

2b875dc9172132d4d4100fb29e903a0aa36d8c13
Tyler <tyler.rinker@gmail.com>
2017-02-05 15:07:39 -0500

    * `as_dtm` and `as_tdm` added to coerce objects to **tm** `DocumentTermMatrix`   and `TermDocumentMatrix` data structures.  This will allow other packages to   easily import this function for their objects.

---
+++ R/as_dtm.R
@@ -0,0 +1,81 @@
+#' Coerce Various Object Into a \code{DocumentTermMatrix}/\code{TermDocumentMatrix}
+#'
+#' Convenience functions to convert a objects from different packages into either a \code{tm::DocumentTermMatrix} or \code{tm::TermDocumentMatrix} object.  Grouping variables are used as the row/column names for the \code{DocumentTermMatrix}/\code{TermDocumentMatrix}.
+#'
+#' @param x A data object.
+#' @param weighting A weighting function capable of handling a \code{tm::DocumentTermMatrix}. It defaults to \code{weightTf} for term frequency weighting. Available weighting functions shipped with the \pkg{tm} package are \code{weightTf}, \code{weightTfIdf}, \code{weightBin}, and \code{weightSMART}.
+#' @param \ldots ignored.
+#' @return Returns a \code{tm::DocumentTermMatrix} or \code{tm::TermDocumentMatrix} object.
+#' @keywords documenttermmatrix, termdocumentmatrix
+#' @export
+#' @rdname as_dtm
+#' @examples
+#' x <- 'temp holder'
+as_dtm <- function(x, weighting = tm::weightTf, ...){
+    UseMethod('as_dtm')
+}
+
+
+#' @export
+#' @method as_dtm term_count
+as_dtm.term_count <- function(x, weighting = tm::weightTf, ...){
+    y <- as.matrix(term_cols(x))
+    rownames(y) <- paste2(group_cols(x))
+    tm::as.DocumentTermMatrix(slam::as.simple_triplet_matrix(y), weighting = weighting)
+}
+
+#' @export
+#' @method as_dtm tbl_df
+as_dtm.tbl_df <- function(x, weighting = tm::weightTf, ...){
+    tm::as.DocumentTermMatrix(slam::as.simple_triplet_matrix(as.matrix(x)), weighting = weighting)
+}
+
+
+
+#' @export
+#' @method as_dtm character
+as_dtm.character <- function(x, weighting = tm::weightTf, ...){
+
+    tm::weightTfIdf(q_dtm(x, ...))
+
+}
+
+
+
+
+
+
+
+
+
+#' @export
+#' @rdname as_dtm
+as_tdm <- function(x, weighting = tm::weightTf, ...){
+    UseMethod('as_tdm')
+}
+
+
+#' @export
+#' @method as_tdm term_count
+as_tdm.term_count <- function(x, weighting = tm::weightTf, ...){
+    y <- as.matrix(term_cols(x))
+    rownames(y) <- paste2(group_cols(x))
+    tm::as.TermDocumentMatrix(slam::as.simple_triplet_matrix(t(y)), weighting = weighting)
+}
+
+#' @export
+#' @method as_tdm tbl_df
+as_tdm.tbl_df <- function(x, weighting = tm::weightTf, ...){
+
+    tm::as.TermDocumentMatrix(slam::as.simple_triplet_matrix(as.matrix(x)), weighting = weighting)
+
+}
+
+#' @export
+#' @method as_dtm character
+as_tdm.character <- function(x, weighting = tm::weightTf, ...){
+
+    tm::weightTfIdf(q_tdm(x, ...))
+
+}
+
--- R/utils.R
+++ R/utils.R
@@ -16,3 +16,78 @@ stem <- function(x, language = "porter") SnowballC::wordStem(x, language)
 
     text.var
 }
+
+
+paste2 <- function (multi.columns, sep = ".", handle.na = TRUE, trim = TRUE) {
+    if (is.matrix(multi.columns)) {
+        multi.columns <- data.frame(multi.columns, stringsAsFactors = FALSE)
+    }
+    if (trim)
+        multi.columns <- lapply(multi.columns, function(x) {
+            gsub("^\\s+|\\s+$", "", x)
+        })
+    if (!is.data.frame(multi.columns) & is.list(multi.columns)) {
+        multi.columns <- do.call("cbind", multi.columns)
+    }
+    if (handle.na) {
+        m <- apply(multi.columns, 1, function(x) {
+            if (any(is.na(x))) {
+                NA
+            } else {
+                paste(x, collapse = sep)
+            }
+        })
+    } else {
+        m <- apply(multi.columns, 1, paste, collapse = sep)
+    }
+    names(m) <- NULL
+    return(m)
+}
+
+term_cols <- function(x, ...){
+
+    terms <- ifelse(inherits(x, 'token_count'), "token.vars", "term.vars")
+    type <- ifelse(inherits(x, 'token_count'), "token", "term")
+
+    y <- validate_term_count(x, FALSE)
+    if (!isTRUE(y)) stop(paste0('`x` does not appear to be a valid `', type, '_count` object.  Was the object altered after creation?'))
+    x[unlist(attributes(x)[[terms]])]
+
+}
+
+
+group_cols <- function(x, ...){
+
+    type <- ifelse(inherits(x, 'token_count'), "token", "term")
+
+    y <- validate_term_count(x, FALSE)
+    if (!isTRUE(y)) stop(paste0('`x` does not appear to be a valid `', type, '_count` object.  Was the object altered after creation?'))
+    x[unlist(attributes(x)[['group.vars']])]
+
+}
+
+validate_term_count <- function(x, warn = FALSE, ...){
+
+    terms <- ifelse(inherits(x, 'token_count'), "token.vars", "term.vars")
+    nwords <- ifelse(inherits(x, 'token_count'), "n.tokens", "n.words")
+    type <- ifelse(inherits(x, 'token_count'), "token", "term")
+
+    nms2 <- unlist(list(attributes(x)[[terms]], nwords))
+    nms <- unlist(list(attributes(x)[["group.vars"]], nms2))
+    check <- all(nms %in% colnames(x)) && all(sapply(x[, nms2], is.numeric))
+    check2 <- all(sapply(c("group.vars", terms, "weight", "pretty"), function(y){
+        !is.null(attributes(x)[[y]])
+    }))
+    check3 <- !any(colnames(x) %in% c(nms2, nms, nwords))
+    if (!check | !check2 | check3) {
+        if (isTRUE(warn)){
+            warning(paste0("Does not appear to be a `", type, "_count` object.\n"),
+                "  Has the object or column names been altered/added?",
+                immediate. = TRUE
+            )
+        }
+        return(FALSE)
+    }
+    TRUE
+}
+

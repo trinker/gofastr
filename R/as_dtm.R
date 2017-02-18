2282e1b667eb44c0d7220ee9102fa6b4fa9d922f
Tyler <tyler.rinker@gmail.com>
2017-02-17 20:55:52 -0500

    made as_dtm.tbl_df to as_dtm_data.frame and added as_dtm.matrix

--- R/as_dtm.R
+++ R/as_dtm.R
@@ -5,6 +5,10 @@
 #' @param x A data object.
 #' @param weighting A weighting function capable of handling a \code{tm::DocumentTermMatrix}. It defaults to \code{weightTf} for term frequency weighting. Available weighting functions shipped with the \pkg{tm} package are \code{weightTf}, \code{weightTfIdf}, \code{weightBin}, and \code{weightSMART}.
 #' @param \ldots ignored.
+#' @param docs The vector of integers or character strings denoting document
+#' columns.
+#' @param pos logical.  If \code{TRUE} parts of speech will be used.  If
+#' \code{FALSE} the corresponding tokens will be used.
 #' @return Returns a \code{tm::DocumentTermMatrix} or \code{tm::TermDocumentMatrix} object.
 #' @keywords documenttermmatrix, termdocumentmatrix
 #' @export
@@ -13,6 +17,9 @@
 #' with(partial_republican_debates_2015,
 #'     as_dtm(dialogue, paste(location, element_id, sentence_id, sep = "_"))
 #' )
+#'
+#' as_dtm(mtcars)
+#' as_dtm(CO2, docs = c('Plant', 'Type', 'Treatment'))
 #' \dontrun{
 #' ## termco object to DTM/TDM
 #' library(termco)
@@ -51,30 +58,61 @@
 #'     as_basic() %>%
 #'     as_dtm()
 #' }
-as_dtm <- function(x, weighting = tm::weightTf, ...){
+as_dtm <- function(x, weighting = tm::weightTf, docs = NULL, pos = TRUE, ...){
     UseMethod('as_dtm')
 }
 
 
 #' @export
 #' @method as_dtm term_count
-as_dtm.term_count <- function(x, weighting = tm::weightTf, ...){
+as_dtm.term_count <- function(x, weighting = tm::weightTf, docs = NULL, pos = TRUE, ...){
     y <- as.matrix(term_cols(x))
     rownames(y) <- paste2(group_cols(x))
     tm::as.DocumentTermMatrix(slam::as.simple_triplet_matrix(y), weighting = weighting)
 }
 
+# # @export
+# # @method as_dtm tbl_df
+# as_dtm.tbl_df <- function(x, weighting = tm::weightTf, ...){
+#     tm::as.DocumentTermMatrix(slam::as.simple_triplet_matrix(as.matrix(x)), weighting = weighting)
+# }
+
+
+
 #' @export
-#' @method as_dtm tbl_df
-as_dtm.tbl_df <- function(x, weighting = tm::weightTf, ...){
-    tm::as.DocumentTermMatrix(slam::as.simple_triplet_matrix(as.matrix(x)), weighting = weighting)
+#' @method as_dtm data.frame
+as_dtm.data.frame <- function(x, weighting = tm::weightTf, docs = NULL, pos = TRUE, ...){
+
+    x <- as.data.frame(x, stringsAsFactors = FALSE)
+    if (!is.null(docs)) {
+        if (is.character(docs)) docs <- which(colnames(x) %in% docs)
+        nms <- paste2(x[, docs, drop = FALSE], ...)
+        ndocs <- setdiff(seq_len(ncol(x)), docs)
+    } else {
+        ndocs <- seq_len(ncol(x))
+        nms <- seq_len(nrow(x))
+    }
+
+    stopifnot (!any(!unlist(lapply(x[, ndocs, drop=FALSE], function(y) is.numeric(y) | is.logical(y)))))
+    out <- tm::as.DocumentTermMatrix(slam::as.simple_triplet_matrix(as.matrix(x[, ndocs, drop=FALSE])), weighting = weighting)
+    rownames(out) <- nms
+    out
+
 }
 
+
+#' @export
+#' @method as_dtm matrix
+as_dtm.matrix <- function(x, weighting = tm::weightTf, docs = NULL, pos = TRUE, ...){
+    stopifnot (!any(!unlist(lapply(x, function(y) is.numeric(y) | is.logical(y)))))
+    tm::as.DocumentTermMatrix(slam::as.simple_triplet_matrix(x), weighting = weighting)
+
+}
 
 
 #' @export
 #' @method as_dtm character
-as_dtm.character <- function(x, weighting = tm::weightTf, ...){
+as_dtm.character <- function(x, weighting = tm::weightTf, docs = NULL, pos = TRUE, ...){
 
     tm::weightTfIdf(q_dtm(x, ...))
 
@@ -84,7 +122,7 @@ as_dtm.character <- function(x, weighting = tm::weightTf, ...){
 
 #' @export
 #' @method as_dtm count_tags
-as_dtm.count_tags <- function(x, weighting = tm::weightTf, ...){
+as_dtm.count_tags <- function(x, weighting = tm::weightTf, docs = NULL, pos = TRUE, ...){
 
     atts <- attributes(x)
     class(x) <- class(x)[!class(x) %in% "count_tags"]
@@ -103,7 +141,7 @@ as_dtm.count_tags <- function(x, weighting = tm::weightTf, ...){
 
 #' @export
 #' @method as_dtm tag_pos
-as_dtm.tag_pos <- function(x, weighting = tm::weightTf, pos = TRUE, ...){
+as_dtm.tag_pos <- function(x, weighting = tm::weightTf, docs = NULL, pos = TRUE, ...){
 
     if (isTRUE(pos)) x <- get_pos(x) else x <- get_tokens(x)
     tm::weightTfIdf(q_dtm(x, ...))
@@ -111,32 +149,70 @@ as_dtm.tag_pos <- function(x, weighting = tm::weightTf, pos = TRUE, ...){
 }
 
 
+
+
+
+
 #' @export
 #' @rdname as_dtm
-as_tdm <- function(x, weighting = tm::weightTf, ...){
+as_tdm <- function(x, weighting = tm::weightTf, docs = NULL, pos = TRUE, ...){
     UseMethod('as_tdm')
 }
 
 
 #' @export
 #' @method as_tdm term_count
-as_tdm.term_count <- function(x, weighting = tm::weightTf, ...){
+as_tdm.term_count <- function(x, weighting = tm::weightTf, docs = NULL, pos = TRUE, ...){
     y <- as.matrix(term_cols(x))
     rownames(y) <- paste2(group_cols(x))
     tm::as.TermDocumentMatrix(slam::as.simple_triplet_matrix(t(y)), weighting = weighting)
 }
 
-#' @export
-#' @method as_tdm tbl_df
-as_tdm.tbl_df <- function(x, weighting = tm::weightTf, ...){
+# # @export
+# # @method as_tdm tbl_df
+# as_tdm.tbl_df <- function(x, weighting = tm::weightTf, ...){
+#
+#     tm::as.TermDocumentMatrix(slam::as.simple_triplet_matrix(as.matrix(x)), weighting = weighting)
+#
+# }
 
-    tm::as.TermDocumentMatrix(slam::as.simple_triplet_matrix(as.matrix(x)), weighting = weighting)
+#' @export
+#' @method as_dtm data.frame
+as_tdm.data.frame <- function(x, weighting = tm::weightTf, docs = NULL, pos = TRUE, ...){
+
+    x <- as.data.frame(x, stringsAsFactors = FALSE)
+    if (!is.null(docs)) {
+        if (is.character(docs)) docs <- which(colnames(x) %in% docs)
+        nms <- paste2(x[, docs, drop = FALSE], ...)
+        ndocs <- setdiff(seq_len(ncol(x)), docs)
+    } else {
+        ndocs <- seq_len(ncol(x))
+        nms <- seq_len(nrow(x))
+    }
+
+    stopifnot (!any(!unlist(lapply(x[, ndocs, drop=FALSE], function(y) is.numeric(y) | is.logical(y)))))
+    out <- tm::as.TermDocumentMatrix(slam::as.simple_triplet_matrix(as.matrix(x[, ndocs, drop=FALSE])), weighting = weighting)
+    rownames(out) <- nms
+    out
 
 }
 
+
+#' @export
+#' @method as_dtm matrix
+as_tdm.matrix<- function(x, weighting = tm::weightTf, docs = NULL, pos = TRUE, ...){
+    stopifnot (!any(!unlist(lapply(x, function(y) is.numeric(y) | is.logical(y)))))
+    tm::as.TermDocumentMatrix(slam::as.simple_triplet_matrix(x), weighting = weighting)
+
+}
+
+
+
+
+
 #' @export
 #' @method as_dtm character
-as_tdm.character <- function(x, weighting = tm::weightTf, ...){
+as_tdm.character <- function(x, weighting = tm::weightTf, docs = NULL, pos = TRUE, ...){
 
     tm::weightTfIdf(q_tdm(x, ...))
 
@@ -145,7 +221,7 @@ as_tdm.character <- function(x, weighting = tm::weightTf, ...){
 
 #' @export
 #' @method as_tdm count_tags
-as_tdm.count_tags <- function(x, weighting = tm::weightTf, ...){
+as_tdm.count_tags <- function(x, weighting = tm::weightTf, docs = NULL, pos = TRUE, ...){
 
     atts <- attributes(x)
     class(x) <- class(x)[!class(x) %in% "count_tags"]
@@ -165,7 +241,7 @@ as_tdm.count_tags <- function(x, weighting = tm::weightTf, ...){
 
 #' @export
 #' @method as_tdm tag_pos
-as_tdm.tag_pos <- function(x, weighting = tm::weightTf, pos = TRUE, ...){
+as_tdm.tag_pos <- function(x, weighting = tm::weightTf, docs = NULL, pos = TRUE,  ...){
 
     if (isTRUE(pos)) x <- get_pos(x) else x <- get_tokens(x)
     tm::weightTfIdf(q_tdm(x, ...))
